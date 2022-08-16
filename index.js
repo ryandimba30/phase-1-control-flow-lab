e0746b28fc0cbf830ceb82fa8957294ec8bf490d
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-16 13:32:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  //Write your code here!
+  if (feet<=400) {
+    return 'This one is on me!';
+  }
+  if (feet >2500) {
+    return 'No can do.';
+  }else if (feet >=2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  }
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.';
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        default:
+          return 'Bye.'
+  }
 }
